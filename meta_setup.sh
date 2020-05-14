3127be90ba6aafe8031371b2a40f50e8b319b5ec
MU-editech <mu.editech@gmail.com>
2020-05-15 02:37:04 +0900

    initial commit

---
+++ meta_setup.sh
@@ -0,0 +1,6 @@
+# 参考：https://coroccu.com/codeigniter-nginx-with-docker/
+git clone https://github.com/coroccu/codeigniter-nginx-with-docker.git
+rm -rf codeigniter-nginx-with-docker/.git
+git init
+git add -A
+git commit -m "initial commit"
